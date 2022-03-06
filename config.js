7e59e2cec32f4cc7cfc4658ae7673b1bdfe331f2
SuryaYoga <86194154+SuryaYoga@users.noreply.github.com>
2022-03-06 23:46:53 +0800

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6285738454808','6285738454808','6288292024190']
+global.premium = ['6285738453808']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
